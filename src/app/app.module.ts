a52e95e94419fb26963e747bced465a574abb527
Rogier <rogiervandenberg@gmail.com>
2017-03-20 23:26:19 +0100

    ADD pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,34 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AuthHttp, AuthConfig } from 'angular2-jwt';
+import { Http } from '@angular/http';
+import { Storage } from '@ionic/storage';
+
+import {WorkPage} from '../pages/work/work';
+import {LoginPage} from '../pages/login/login';
+
+import {AuthService} from '../providers/auth-service';
+import {DataService} from '../providers/data-service';
+
+
+
+let storage = new Storage();
+
+export function getAuthHttp(http) {
+  return new AuthHttp(new AuthConfig({
+    noJwtError: true,
+    globalHeaders: [{'Accept': 'application/json'}],
+    tokenGetter: (() => storage.get('id_token')),
+  }), http);
+}
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage, 
+    LoginPage,
+    WorkPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,8 +37,22 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    WorkPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [
+    {
+      provide: ErrorHandler,
+      useClass: IonicErrorHandler
+    },
+    {
+      provide: AuthHttp,
+      useFactory: getAuthHttp,
+      deps: [Http]
+    },
+    AuthService,
+    DataService
+  ]
 })
 export class AppModule {}
